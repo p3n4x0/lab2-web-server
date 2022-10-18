d0dd60e6d5172aa1731f21c7f11bff6c9e707af4
p3n4x0 <penacho.ae@gmail.com>
2022-10-18 18:19:08 +0200

    final push

---
+++ src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
@@ -0,0 +1,25 @@
+package es.unizar.webeng.lab2
+
+import java.time.LocalDateTime
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+class TimeDTO(val time: LocalDateTime)
+
+interface TimeProvider {
+    fun now(): LocalDateTime
+  }
+
+@Service
+class TimeService: TimeProvider {
+override fun now() = LocalDateTime.now()
+}
+
+fun LocalDateTime.toDTO() = TimeDTO(time = this)
+
+@RestController 
+class TimeController(val service: TimeProvider) {
+  @GetMapping("/time")
+  fun time() = service.now().toDTO()
+}
